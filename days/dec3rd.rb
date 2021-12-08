85fa4571c5a6175a90285d9b06c8407873fac0e0
Shannarra <petarangelov15@gmail.com>
2021-12-08 14:37:59 +0200

    Solved 3rd day challenge part 2

--- days/dec3rd.rb
+++ days/dec3rd.rb
@@ -22,28 +22,33 @@ end
 p solve binaries
 
 def solvePart2(binaries)
+
+  def mask_for(selection, index)
+    mask = ''
+    selection.each{|x| mask += x[index] }
+    mask
+  end
+
   index = 0
 
   o2 = binaries
-  co2 = binaries
-  puts "a #{index} #{o2.count} #{co2.count}"
+  co2 = binaries.clone
 
-  until o2.count == 1 && co2.count == 1
-    _col_mask = ''
-    (0...binaries.length).each do |row|
-      _col_mask += binaries[row][index]
-    end
+  until o2.count == 1 && co2.count
+    _col_mask = mask_for(o2, index)
 
     most = _col_mask.count('1') >= _col_mask.count('0') ? '1' : '0'
-    least = (!!most[index]).to_s.to_i.to_s
-    
+
+    _col_mask = mask_for(co2, index)
+    least = _col_mask.count('1') >= _col_mask.count('0') ? '0' : '1'
+
     o2.select!{|x| x[index] == most } if o2.count > 1
-    co2.select!{|x| x[index] == least } if co2.count > 1
+    co2.select!{|x| x[index] == least} if co2.count > 1
 
     index += 1
-    #puts "a #{index} #{o2.count} #{co2.count}"
   end
-  [o2, co2]
+
+  o2.first.to_i(2) * co2.first.to_i(2)
 end
 
 p solvePart2 binaries
